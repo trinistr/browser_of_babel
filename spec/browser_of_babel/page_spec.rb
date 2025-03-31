258109159be0a2542ef33c9c6278cde71166657c
Alexandr Bulancov <6594487+trinistr@users.noreply.github.com>
2025-03-31 22:58:48 +0300

    Add a test for Page#to_url

--- spec/browser_of_babel/page_spec.rb
+++ spec/browser_of_babel/page_spec.rb
@@ -12,4 +12,28 @@ RSpec.describe BrowserOfBabel::Page, :aggregate_failures do
   it "has a depth of 5" do
     expect(described_class.depth).to be 5
   end
+
+  describe "#to_url" do
+    subject(:uri) { page.to_url }
+
+    let(:library) { "https://libraryofbabel.info/book.cgi" }
+    let(:babel) { BrowserOfBabel }
+    let(:hex) { SecureRandom.hex }
+    let(:wall) { rand(1..4) }
+    let(:shelf) { rand(1..5) }
+    let(:volume) { rand(1..32) }
+    let(:page) do
+      h_hex = babel::Hex.new(babel::Library.new, hex)
+      h_wall = babel::Wall.new(h_hex, wall)
+      h_shelf = babel::Shelf.new(h_wall, shelf)
+      h_volume = babel::Volume.new(h_shelf, volume)
+      described_class.new(h_volume, rand(1..410))
+    end
+
+    it "generates properly formatted URL" do
+      expect(uri).to eq(
+        "#{library}?#{hex}-w#{wall}-s#{shelf}-v#{format("%02d", volume)}:#{page.number}"
+      )
+    end
+  end
 end
